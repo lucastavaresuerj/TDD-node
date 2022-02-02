63c5fbe12e3076428c55ef2ee5d35deb4fd33f73
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-02 14:28:05 -0300

    Adding more tests to the add parser, and fixing regex of add

--- src/parser/add/add.parser.spec.js
+++ src/parser/add/add.parser.spec.js
@@ -1,7 +1,32 @@
 import addParser from "./add.parser.js";
 
 describe("Parser of add", () => {
-  it("Should check for sum operation and return the two operators", () => {
-    expect(addParser("1 + 1")).toEqual(["1", "1"]);
+  const testData = {
+    pass: [
+      { input: "165+79", result: ["165", "79"] },
+      { input: "861+ 981231", result: ["861", "981231"] },
+      { input: "681 + 68651+ 6886151", result: ["681", "68651+ 6886151"] },
+      { input: "1+2+3+8", result: ["1", "2+3+8"] },
+      { input: "2   + 984", result: ["2", "984"] },
+    ],
+    fail: [
+      { input: "+165+79", result: ["165", "79"] },
+      { input: "861 981231", result: ["861", "981231"] },
+    ],
+  };
+  it("Should return the two side operators", () => {
+    const { pass } = testData;
+
+    pass.map(({ input, result }) => {
+      expect(addParser(input)).toEqual(result);
+    });
+  });
+
+  it("Should not return the two side operators", () => {
+    const { fail } = testData;
+
+    fail.map(({ input, result }) => {
+      expect(addParser(input)).not.toEqual(result);
+    });
   });
 });

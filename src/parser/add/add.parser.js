fb32bc4ec4625279b230a3375d8de9934cf1b708
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-02 13:43:43 -0300

    Making the regex of parser for add more expressive

--- src/parser/add/add.parser.js
+++ src/parser/add/add.parser.js
@@ -1,5 +1,5 @@
 function addParser(operation = "") {
-  const divideLeftRight = /(.*?) ?\+ ?(.*)/gi; // if "165 + 897 + 8186" then left is "165" and right is "897 + 8186"
+  const divideLeftRight = /(\d+?) ?\+ ?(.*)/gi; // if "165 + 897 + 8186" then left is "165" and right is "897 + 8186"
 
   return operation
     .replace(divideLeftRight, (math, p1, p2) => {

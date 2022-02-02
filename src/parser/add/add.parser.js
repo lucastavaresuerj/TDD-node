e39632fc81b29d3193bccb97269d1b1a6587d68c
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-02 13:40:18 -0300

    Adding logic to add.parser

--- src/parser/add/add.parser.js
+++ src/parser/add/add.parser.js
@@ -1,5 +1,11 @@
-function addParser(operation) {
-  return ["1", "1"];
+function addParser(operation = "") {
+  const divideLeftRight = /(.*?) ?\+ ?(.*)/gi; // if "165 + 897 + 8186" then left is "165" and right is "897 + 8186"
+
+  return operation
+    .replace(divideLeftRight, (math, p1, p2) => {
+      return [p1, p2];
+    })
+    .split(",");
 }
 
 export default addParser;

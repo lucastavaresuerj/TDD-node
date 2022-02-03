f0513af7ceeaf54e1fc04c90b5ac58b94dce0573
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-03 10:30:36 -0300

    Adding a more complex logic for parentheses parser

--- src/parser/parentheses/parentheses.js
+++ src/parser/parentheses/parentheses.js
@@ -1,5 +1,3 @@
-parenthesesParser("1654 + (2 + (3 + (256 - 12)) + 4) + 8 - (26 - 10) - 5");
-
 function parenthesesParser(operation = "") {
   const separatorBlocks = { "(": ")", "[": "]", "{": "}" };
   const keys = {
@@ -8,43 +6,55 @@ function parenthesesParser(operation = "") {
   };
   const parenthesesPile = [];
 
-  return operation.split("").reduce(
-    (acc, value) => {
+  const parsed = operation.split("").reduce(
+    (acc, value, index) => {
       const accIndex = acc.length - 1;
 
       if (keys.open.includes(value)) {
+        const pileLength = parenthesesPile.length;
         parenthesesPile.push(value);
-        acc.push({ p: "" });
-        return [...acc];
-      }
-      if (keys.close.includes(value)) {
-        const close = parenthesesPile.pop(value);
-        if (parenthesesPile.length == 0) {
-          acc.push("");
+        if (pileLength == 0) {
+          if (index == 0) {
+            acc[accIndex] = { p: "" };
+          } else {
+            acc.push({ p: "" });
+          }
         } else {
-          acc[accIndex].p = acc[accIndex].p + close;
+          acc[accIndex].p = acc[accIndex].p + value;
         }
         return [...acc];
       }
-
-      if (acc[accIndex].p) {
+      if (keys.close.includes(value)) {
+        const openSeparator = parenthesesPile.pop();
+        if (value != separatorBlocks[openSeparator]) {
+          throw new Error(
+            "Matching close separator different from the open separator"
+          );
+        }
+        if (parenthesesPile.length == 0) {
+          if (index + 1 != operation.length) {
+            acc.push("");
+          }
+        } else {
+          acc[accIndex].p = acc[accIndex].p + value;
+        }
+        return [...acc];
+      }
+      if (typeof acc[accIndex].p == "string") {
         acc[accIndex].p = acc[accIndex].p + value;
       } else {
         acc[accIndex] = acc[accIndex] ? acc[accIndex] + value : value;
       }
-
       return [...acc];
     },
     [""]
   );
 
-  return [
-    "1654 + ",
-    { p: "2 + (3 + (256 - 12)) + 4" },
-    " + 8 - ",
-    { p: "26 - 10" },
-    " - 5",
-  ];
+  if (parenthesesPile.length) {
+    throw new Error("Number of parentheses did not match");
+  }
+
+  return parsed;
 }
 
 export default parenthesesParser;

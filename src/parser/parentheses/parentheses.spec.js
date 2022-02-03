bad5cb402701f95e7a67e4983aa587d6d4cc2e56
Lucas Tavares <lucastavaresuerj2016@gmail.com>
2022-02-03 10:30:00 -0300

    Adding more tests for parentheses parser

--- src/parser/parentheses/parentheses.spec.js
+++ src/parser/parentheses/parentheses.spec.js
@@ -1,15 +1,42 @@
 import parenthesesParser from "./parentheses";
 
 describe("Parentheses", () => {
+  const testData = {
+    pass: [
+      {
+        input: "165 + 85",
+        result: ["165 + 85"],
+      },
+      {
+        input: "(165 + 85)",
+        result: [{ p: "165 + 85" }],
+      },
+
+      {
+        input: "((((165 + 85))))",
+        result: [{ p: "(((165 + 85)))" }],
+      },
+      {
+        input: "(165 + 85) + 8",
+        result: [{ p: "165 + 85" }, " + 8"],
+      },
+      {
+        input: "1654 + (2 + (3 + (256 - 12)) + 4) + 8 - (26 - 10) - 5",
+        result: [
+          "1654 + ",
+          { p: "2 + (3 + (256 - 12)) + 4" },
+          " + 8 - ",
+          { p: "26 - 10" },
+          " - 5",
+        ],
+      },
+    ],
+  };
   it("Should separate in the bigger parentheses", () => {
-    expect(
-      parenthesesParser("1654 + (2 + (3 + (256 - 12)) + 4) + 8 - (26 - 10) - 5")
-    ).toEqual([
-      "1654 + ",
-      { p: "2 + (3 + (256 - 12)) + 4" },
-      " + 8 - ",
-      { p: "26 - 10" },
-      " - 5",
-    ]);
+    const { pass } = testData;
+
+    pass.map(({ input, result }) => {
+      expect(parenthesesParser(input)).toEqual(result);
+    });
   });
 });
